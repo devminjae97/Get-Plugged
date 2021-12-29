4a34a1a863d4feba5f18d721c355d3cfb8992987
yangsungin <didtjddls96@naver.com>
2021-12-29 16:12:36 +0900

    예외처리, list:Flag -> Stage

--- twoweep/Assets/Scripts/GameManager.cs
+++ twoweep/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour 
 { 
 
@@ -9,7 +10,7 @@ public class GameManager : MonoBehaviour
     private PlayerController playerController2;
 
     // fix : yang 2021-12-29
-    public List<GameObject> startFlags = new List<GameObject>();
+    public List<GameObject> stages = new List<GameObject>();
     
     void Start() 
     {
@@ -17,20 +18,34 @@ public class GameManager : MonoBehaviour
         playerController2 = GameObject.Find("Player2").GetComponent<PlayerController>();
 
         // fix : yang 2021-12-29
-        playerController1.transform.position = startFlags[LevelSelect.thisLevel * 2 - 2].transform.position;
-        playerController2.transform.position = startFlags[LevelSelect.thisLevel * 2 - 1].transform.position;
+        try
+        {
+            playerController1.transform.position = stages[LevelSelect.thisLevel - 1].transform.Find("StartFlag1").transform.position;
+            playerController2.transform.position = stages[LevelSelect.thisLevel - 1].transform.Find("StartFlag2").transform.position;
+        }
+        catch (System.ArgumentOutOfRangeException e)
+        {
+            Debug.Log("Stage is not selected. Current Stage is Null.");
+        }
     }
 
     void Update()
     {
         if (IsEndStage()) 
         {
-            Debug.Log("Done");
+            // Debug.Log("Done");
 
             // fix : yang 2021-12-29
-            playerController1.transform.position = startFlags[(LevelSelect.thisLevel + 1) * 2 - 2].transform.position;
-            playerController2.transform.position = startFlags[(LevelSelect.thisLevel + 1) * 2 - 1].transform.position;
-            LevelSelect.thisLevel += 1;
+            try
+            {
+                playerController1.transform.position = stages[LevelSelect.thisLevel].transform.Find("StartFlag1").transform.position;
+                playerController2.transform.position = stages[LevelSelect.thisLevel].transform.Find("StartFlag2").transform.position;
+                LevelSelect.thisLevel += 1;
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                Debug.Log("Lack of stages Array. Stage Array should be added.");
+            }
         }
     }
 
