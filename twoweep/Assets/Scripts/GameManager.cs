35d682cb8c407b596e2ceb39c87c0c25d7106ec6
yangsungin <didtjddls96@naver.com>
2021-12-29 14:04:09 +0900

    Select Stage And Stage Clear

--- twoweep/Assets/Scripts/GameManager.cs
+++ twoweep/Assets/Scripts/GameManager.cs
@@ -7,19 +7,30 @@ public class GameManager : MonoBehaviour
 
     private PlayerController playerController1;
     private PlayerController playerController2;
+
+    // fix : yang 2021-12-29
+    public List<GameObject> startFlags = new List<GameObject>();
     
     void Start() 
     {
         playerController1 = GameObject.Find("Player1").GetComponent<PlayerController>();
         playerController2 = GameObject.Find("Player2").GetComponent<PlayerController>();
+
+        // fix : yang 2021-12-29
+        playerController1.transform.position = startFlags[LevelSelect.thisLevel * 2 - 2].transform.position;
+        playerController2.transform.position = startFlags[LevelSelect.thisLevel * 2 - 1].transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (IsEndStage()) 
         {
             Debug.Log("Done");
+
+            // fix : yang 2021-12-29
+            playerController1.transform.position = startFlags[(LevelSelect.thisLevel + 1) * 2 - 2].transform.position;
+            playerController2.transform.position = startFlags[(LevelSelect.thisLevel + 1) * 2 - 1].transform.position;
+            LevelSelect.thisLevel += 1;
         }
     }
 
