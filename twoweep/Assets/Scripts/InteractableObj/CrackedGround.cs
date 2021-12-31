14bd8ca03ef508f8664602842354206050a75ab9
yangsungin <didtjddls96@naver.com>
2021-12-31 13:10:23 +0900

    Interactor_CrackedGround

---
+++ twoweep/Assets/Scripts/InteractableObj/CrackedGround.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackedGround : MonoBehaviour
+{
+    public float timeToBreak;
+    public float timeToRespawn;
+    IEnumerator BreakGround()
+    {
+        yield return new WaitForSeconds(timeToBreak);
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        yield return new WaitForSeconds(timeToRespawn);
+        gameObject.GetComponent<SpriteRenderer>().enabled = true;
+        gameObject.GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine("BreakGround");
+        }
+    }
+}
