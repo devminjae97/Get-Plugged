e8daf485f8cabeccf09218ed46e84cd8fd856ba8
yangsungin <didtjddls96@naver.com>
2022-01-03 01:19:17 +0900

    laser_fix

--- twoweep/Assets/Scripts/InteractableObj/INT_Laser.cs
+++ twoweep/Assets/Scripts/InteractableObj/INT_Laser.cs
@@ -17,11 +17,14 @@ public class INT_Laser : MonoBehaviour
     private Transform blockingLaserTileChildTransform;
 
     float startPosX, startPosY, endPosX, endPosY;
-    private bool isInit;
+    private SpriteRenderer sr;
+    public Sprite RotateImage;
 
     private void Start()
     {
         bc = GetComponent<BoxCollider2D>();
+        sr = gameObject.transform.parent.GetComponent<SpriteRenderer>();
+
         if (blockingLaserTileParent.childCount > 0)
         {
             for (int i = 0; i < blockingLaserTileParent.childCount; i++)
@@ -41,18 +44,24 @@ public class INT_Laser : MonoBehaviour
             case LaserDirection.Top:
                 lineRenderer.SetPosition(1, blockingLaserTileParent.GetChild(0).transform.position);
                 blockingLaserTileChildTransform = blockingLaserTileParent.GetChild(0).transform;
+                sr.flipX = false;
                 break;
             case LaserDirection.Right:
                 lineRenderer.SetPosition(1, blockingLaserTileParent.GetChild(1).transform.position);
                 blockingLaserTileChildTransform = blockingLaserTileParent.GetChild(1).transform;
+                sr.sprite = RotateImage;
+                sr.flipX = false;
                 break;
             case LaserDirection.Bottom:
                 lineRenderer.SetPosition(1, blockingLaserTileParent.GetChild(2).transform.position);
                 blockingLaserTileChildTransform = blockingLaserTileParent.GetChild(2).transform;
+                sr.flipX = true;
                 break;
             case LaserDirection.Left:
                 lineRenderer.SetPosition(1, blockingLaserTileParent.GetChild(3).transform.position);
                 blockingLaserTileChildTransform = blockingLaserTileParent.GetChild(3).transform;
+                sr.sprite = RotateImage;
+                sr.flipX = true;
                 break;
             default:
                 break;
@@ -70,24 +79,24 @@ public class INT_Laser : MonoBehaviour
         Vector2 tmpPos = new Vector2(Mathf.Abs(startPosX - endPosX), Mathf.Abs(startPosY - endPosY));
         Vector2 tmpPosNotAbs = new Vector2(startPosX - endPosX, startPosY - endPosY);
 
-        lineRenderer.SetPosition(1, blockingLaserTileChildTransform.position);
-
         if (tmpPos.x == 0 || laserDir == LaserDirection.Top || laserDir == LaserDirection.Bottom)
         {
             tmpPos.x = 0.1f;
             boxCollider.offset = new Vector2(0.0f, -tmpPosNotAbs.y / 2);
+            lineRenderer.SetPosition(1, new Vector2(startObject.position.x, endObject.position.y));
         }
         else if (tmpPos.y == 0 || laserDir == LaserDirection.Right || laserDir == LaserDirection.Left)
         {
             tmpPos.y = 0.1f;
             boxCollider.offset = new Vector2(-tmpPosNotAbs.x / 2, 0.0f);
+            lineRenderer.SetPosition(1, new Vector2(endObject.position.x, startObject.position.y));
         }
         boxCollider.size = tmpPos;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") && collision.transform.parent != this)
+        if (collision.gameObject.CompareTag("Ground") && this.transform.parent.gameObject != collision.gameObject)
         {
             SetBoxCollider2D(gameObject.transform, collision.gameObject.transform, bc);
         }
@@ -95,7 +104,7 @@ public class INT_Laser : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") && collision.transform.parent != this)
+        if (collision.gameObject.CompareTag("Ground") && this.transform.parent.gameObject != collision.gameObject)
         {
             SetBoxCollider2D(gameObject.transform, blockingLaserTileChildTransform.gameObject.transform, bc);
         }
